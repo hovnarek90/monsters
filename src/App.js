412e4e24f626f431e8a6241828915b1cfd5e3280
Narek Hovhannisyan <hovnarek@gmail.com>
2022-11-15 15:03:12 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Linda",
+          id: "12e43323",
+        },
+        {
+          name: "Frank",
+          id: "12e4323",
+        },
+        {
+          name: "Jacky",
+          id: "12e43d23",
+        },
+        {
+          name: "Andrei",
+          id: "12e4w323",
+        },
+        
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
